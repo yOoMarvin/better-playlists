984b8b9e047ae72c2f9d945bc89f0a57bb9d3796
yOoMarvin <marvin.messenzehl@gmail.com>
2018-02-09 15:01:16 +0100

    Added subfetches

--- src/App.js
+++ src/App.js
@@ -136,12 +136,38 @@ class App extends Component {
     fetch('https://api.spotify.com/v1/me/playlists', {
       headers: {'Authorization': 'Bearer ' + acessToken}
     }).then(response => response.json())
-    .then(data => this.setState({
-      playlists: data.items.map(item => ({
-        name: item.name,
-        imageUrl: item.images[0].url, 
-        songs: []
-      }))
+    .then(playlistData => {
+      let playlists = playlistData.items
+      let trackDataPromises = playlists.map(playlist => {
+        let responsePromise = fetch(playlist.tracks.href, {
+          headers: {'Authorization': 'Bearer ' + acessToken}
+        })
+        let trackDataPromise = responsePromise
+          .then(response => response.json())
+        return trackDataPromise
+      })
+      let allTracksDataPromises = Promise.all(trackDataPromises)
+      let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+        trackDatas.forEach((trackData, i) => {
+          playlists[i].trackDatas = trackData.items
+            .map(item => item.track)
+            .map(trackData => ({
+              name: trackData.name,
+              duration: trackData.duration_ms / 1000
+            }))
+        })
+        return playlists
+      })
+      return playlistsPromise
+    })
+    .then(playlists => this.setState({
+      playlists: playlists.map(item => {
+        return {
+          name: item.name,
+          imageUrl: item.images[0].url, 
+          songs: item.trackDatas.slice(0,3)
+        }
+    })
     }))
   }
   render() {
